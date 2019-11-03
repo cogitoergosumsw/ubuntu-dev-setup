90d7dfc16e8996675f03d46c812bea64d2e14f52
Seng Wee <sengwee_new@hotmail.com>
2019-11-03 13:30:24 +0800

    auto add PPA for grub-customizer

--- quick_install.sh
+++ quick_install.sh
@@ -67,7 +67,7 @@ echo 'tig installed!'
 # install Grub Customizer
 # (to configure the Grub boot menu easily)
 echo 'Installing Grub Customizer...'
-sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
 sudo apt-get update
 sudo apt-get -y install grub-customizer
 echo 'Grub Customizer Installed!'
